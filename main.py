563e7ba28482431806941d638a8d36c0e037224d
nerazull <dragosnboston@gmail.com>
2024-02-23 16:09:56 +0200

    Added the camera (Lesson 97)

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ from settings import *
 from player import Player
 from car import Car
 
+class AllSprites(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+		self.offset = pygame.math.Vector2()
+		self.bg = pygame.image.load('graphics/main/map.png').convert()
+		self.fg = pygame.image.load('graphics/main/overlay.png').convert_alpha()
+
+	def customized_draw(self):
+
+		# change the offset vector
+		self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+		self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+		# blit the bg
+		display_surface.blit(self.bg,-self.offset)
+
+		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery): 
+			offset_pos = sprite.rect.topleft - self.offset
+			display_surface.blit(sprite.image, offset_pos)
+
+		display_surface.blit(self.fg,-self.offset)
 # basic setup
 pygame.init()
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -10,7 +31,7 @@ pygame.display.set_caption('Frogger')
 clock = pygame.time.Clock()
 
 # groups
-all_sprites = pygame.sprite.Group()
+all_sprites = AllSprites()
 
 # sprites
 player = Player((600,400),all_sprites)
@@ -35,7 +56,8 @@ while True:
 	all_sprites.update(dt)
 
 	# draw
-	all_sprites.draw(display_surface)
+	# all_sprites.draw(display_surface)
+	all_sprites.customized_draw()
 
 	# update the display surface -> drawing the frame
 	pygame.display.update()
